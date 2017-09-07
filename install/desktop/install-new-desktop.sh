a546ea527786a002d5f6f7b5c5e344b89eed8027
fabianormatias <fabianormatias@gmail.com>
2017-09-07 01:45:39 -0300

    alterando o install new desktop

--- install/desktop/install-new-desktop.sh
+++ install/desktop/install-new-desktop.sh
@@ -2,70 +2,53 @@
 
 [ $(sudo groups $USER | grep -c vboxusers) -eq 0 ] && sudo adduser $USER vboxusers
 
+sudo apt-get update
+
 sudo apt-get install -y \
 		ardesia \
-		build-essential \
-		byzanz \
-		clusterssh \
 		colordiff \
 		default-jdk \
 		dkms \
 		docker.io \
+		docker-compose \
+		eclipse \
 		erlang \
 		filezilla \
-		gddrescue \
-		geeqie \
 		gimp \
 		git \
-		gsfonts-other \
-		gsfonts-x11 \
-		gsmartcontrol \
 		htop \
-		indicator-multiload \
 		inkscape \
-		jabref \
 		kolourpaint4 \
 		kompare \
-		linux-headers-generic \
-		linux-source \
-		lm-sensors \
 		maven \
 		meld \
-		mercurial \
-		mingw-w64 \
 		mplayer \
 		myspell-en-gb \
 		myspell-en-us \
 		myspell-pt-br \
 		nfs-common \
 		nmap \
-		octave \
 		okular \
 		openssh-client \
 		openssh-server \
 		pdfgrep \
 		python-gpgme \
+		python3 \
 		r-base-core \
+		rabbitmq-server \
 		rdesktop \
-		smartmontools \
 		sshpass \
-		subversion \
 		terminator \
-		texlive-full \
 		texmaker \
 		tree \
 		unrar \
 		vim \
 		virtualbox \
-		visualvm \
 		vpnc \
 		whois \
 		wine \
-		winetricks \
 		wireshark \
 		xfonts-100dpi \
 		xfonts-75dpi \
 		xfonts-base \
 		xz-utils
-
-		#linux-headers-$(uname -r) \
