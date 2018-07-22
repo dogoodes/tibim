74e1ac14c61ed4dea896d7d31efc07e9249bad7b
fabianormatias <fabianormatias@gmail.com>
2018-07-22 13:07:30 -0300

    include functions in vagrantfile

--- vagrant/config/update-vboxdrv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CERTDIR="/path/to/certs"
-
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 $CERTDIR/vboxdrv.priv $CERTDIR/vboxdrv.der $(modinfo -n vboxdrv)
-
-/sbin/modprobe vboxdrv
